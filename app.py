0118a685cc78e53705afad2cf58adb8cc01e5675
Jihyun Lee <jihyunlee@Jihyunui-MacBookAir.local>
2024-06-21 09:37:43 +0900

    First

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/login.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let container = document.getElementById('container');
+
+    function toggle() {
+        container.classList.toggle('sign-in');
+        container.classList.toggle('sign-up');
+    }
+
+    setTimeout(() => {
+        container.classList.add('sign-in');
+    }, 200);
+});
