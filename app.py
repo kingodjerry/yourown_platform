8fa8fea987d5e922755ce856530d045c7db24ece
Jihyun Lee <jihyunlee@Jihyunui-MacBookAir.local>
2024-06-24 14:08:03 +0900

    login

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, request
+
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+users = {
+    'id1': 'password123',
+    'id2': 'password456'
+}
 
 @app.route('/')
 def Home():
-    return redirect('/login')
+    return render_template('login2.html')
+    # return redirect('/login')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
